b18e55062458128a90dfa32000ec9246d100337b
ThornenSan <khmerthonin@gmail.com>
2021-12-14 21:18:45 +0700

    Circle Spiro

--- Intermediate/turtle_graphic/circle_spiro.py
+++ Intermediate/turtle_graphic/circle_spiro.py
@@ -1,21 +1,24 @@
-import turtle
+import turtle as t
+import random
 
-t = turtle.Turtle()
-
-turtle.bgcolor("black")
-turtle.pensize(3)
-turtle.speed(0)
-
-turtle.shape("turtle")
+tim = t.Turtle()
+t.colormode(255)
+tim.speed("fastest")
 
 
-while True:
-    for i in range(6):
-        for colors in ["red", "blue", "magenta", "green", "yellow", "white"]:
-            turtle.color(colors)
-            turtle.circle(100)
-            turtle.left(10)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
 
-turtle.hideturtle()
-turtle.mainloop()
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
